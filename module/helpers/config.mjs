0ae80eeca83b8c34b8f2e5ccab1dd18adadf87b3
elwinner08 <thomas_pruvost@live.fr>
2024-07-18 14:50:15 +0200

    Création des fichiers de base du GS

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const LAMES_CARDINAL = {};
+
+/**
+ * The set of Ability Scores used within the system.
+ * @type {Object}
+ */
+LAMES_CARDINAL.abilities = {
+  str: 'LAMES_CARDINAL.Ability.Str.long',
+  dex: 'LAMES_CARDINAL.Ability.Dex.long',
+  con: 'LAMES_CARDINAL.Ability.Con.long',
+  int: 'LAMES_CARDINAL.Ability.Int.long',
+  wis: 'LAMES_CARDINAL.Ability.Wis.long',
+  cha: 'LAMES_CARDINAL.Ability.Cha.long',
+};
+
+LAMES_CARDINAL.abilityAbbreviations = {
+  str: 'LAMES_CARDINAL.Ability.Str.abbr',
+  dex: 'LAMES_CARDINAL.Ability.Dex.abbr',
+  con: 'LAMES_CARDINAL.Ability.Con.abbr',
+  int: 'LAMES_CARDINAL.Ability.Int.abbr',
+  wis: 'LAMES_CARDINAL.Ability.Wis.abbr',
+  cha: 'LAMES_CARDINAL.Ability.Cha.abbr',
+};
